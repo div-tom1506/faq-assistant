92e4a1dea92eed9d8581f722d96d8c2d6dbe7907
div-tom1506 <divyanshuk1506@gmail.com>
2025-12-19 17:58:39 +0530

    initial commit

---
+++ src/main/java/com/faq_assistant/FaqAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.faq_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaqAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaqAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faq_assistant/FaqAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faq_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaqAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
