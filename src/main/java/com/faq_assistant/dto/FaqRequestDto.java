b2f7ccd5a6c5c9df32aae5dddbec0cd67be79f95
div-tom1506 <divyanshuk1506@gmail.com>
2025-12-27 20:17:37 +0530

    designed DTO classes

--- src/main/java/com/faq_assistant/dto/FaqRequestDto.java
+++ src/main/java/com/faq_assistant/dto/FaqRequestDto.java
@@ -1,5 +1,19 @@
 package com.faq_assistant.dto;
 
+import java.util.List;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class FaqRequestDto {
 
+	@NotBlank
+	private String question;
+	private String answer;
+	@NotNull
+	private Long categoryId;
+	private List<Long> tagIds;
+	@NotNull
+	private Long userId;
+	
 }
--- src/main/java/com/faq_assistant/dto/FaqResponseDto.java
+++ src/main/java/com/faq_assistant/dto/FaqResponseDto.java
@@ -1,5 +1,15 @@
 package com.faq_assistant.dto;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class FaqResponseDto {
 
+	private Long id;
+	private String question;
+	private String answer;
+	private String category;
+	private List<Long> tag;
+	private String createdBy;
+	private LocalDateTime createdAt;
 }
