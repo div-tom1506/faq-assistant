0eb4923c04da260e791a19b7353f07dbc85114c4
div-tom1506 <divyanshuk1506@gmail.com>
2026-01-05 18:08:31 +0530

    Implemented FAQ controller

--- src/main/java/com/faq_assistant/controller/FaqController.java
+++ src/main/java/com/faq_assistant/controller/FaqController.java
@@ -1,5 +1,83 @@
 package com.faq_assistant.controller;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.faq_assistant.dto.ApiResponse;
+import com.faq_assistant.dto.FaqRequestDto;
+import com.faq_assistant.dto.FaqResponseDto;
+import com.faq_assistant.service.FaqService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/faqs")
 public class FaqController {
 
+	private final FaqService faqService;
+	
+	@PostMapping
+	public ResponseEntity<ApiResponse<FaqResponseDto>> createFaq(
+			@Valid @RequestBody FaqRequestDto requestDto
+	) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(new ApiResponse<>(true, faqService.createFaq(requestDto), "FAQ created succesfully"));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ApiResponse<FaqResponseDto>> getFaq(@PathVariable Long id) {
+		return ResponseEntity.ok(
+				new ApiResponse<>(true, faqService.getFaqById(id), "FAQ fetched successfully")
+		);
+	}
+	
+	@GetMapping
+	public ResponseEntity<ApiResponse<Page<FaqResponseDto>>> searchFaqs (
+			@RequestParam(required = false) String search,
+			@RequestParam(required = false) Long categoryId,
+			@RequestParam(required = false) List<Long> tags,
+			@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "10") int size
+	) {
+		Pageable pageable = PageRequest.of(page, size);
+		return ResponseEntity.ok(
+				new ApiResponse<>(
+						true, 
+						faqService.searchFaqs(search, categoryId, tags, pageable), 
+						"FAQ fetched successfully"
+				)
+		);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<ApiResponse<FaqResponseDto>> updateFaq(
+			@PathVariable Long id,
+			@Valid @RequestBody FaqRequestDto requestDto
+	) {
+		return ResponseEntity.ok(
+				new ApiResponse<>(true, faqService.updateFaq(id, requestDto), "FAQ updated succesfully")
+		);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ApiResponse<Void>> deleteFaq(@PathVariable Long id) {
+		faqService.deleteFaq(id);
+		return ResponseEntity.ok(new ApiResponse<>(true, null, "FAQ deleted successfully"));
+	}
 }
